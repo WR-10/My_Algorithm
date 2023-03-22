319e0cef6d5590d75fdb5198bd473a5f1926c49c
WR-10 <sadfrog@kakao.com>
2023-03-22 09:55:45 +0900

    week5

---
+++ Algorithm_Study/week5/고양이와 개는 몇 마리 있을까.sql
@@ -0,0 +1,16 @@
+-- ANIMAL_INS 테이블은 동물 보호소에 들어온 동물의 정보를 담은 테이블입니다. 
+-- ANIMAL_INS 테이블 구조는 다음과 같으며, ANIMAL_ID, ANIMAL_TYPE, DATETIME, INTAKE_CONDITION, NAME, SEX_UPON_INTAKE는 
+-- 각각 동물의 아이디, 생물 종, 보호 시작일, 보호 시작 시 상태, 이름, 성별 및 중성화 여부를 나타냅니다.
+
+-- ex)
+-- 고양이는 2마리, 개는 1마리 들어왔습니다. 따라서 SQL문을 실행하면 다음과 같이 나와야 합니다.
+
+-- ANIMAL_TYPE	count
+-- Cat	         2
+-- Dog	         1
+
+
+SELECT ANIMAL_TYPE, COUNT(ANIMAL_TYPE) 
+FROM ANIMAL_INS 
+GROUP BY ANIMAL_TYPE 
+ORDER BY ANIMAL_TYPE
---
+++ Algorithm_Study/week5/큰 수 만들기.py
@@ -0,0 +1,22 @@
+# 어떤 숫자에서 k개의 수를 제거했을 때 얻을 수 있는 가장 큰 숫자를 구하려 합니다.
+
+# 예를 들어, 숫자 1924에서 수 두 개를 제거하면 [19, 12, 14, 92, 94, 24] 를 만들 수 있습니다. 
+# 이 중 가장 큰 숫자는 94 입니다.
+
+
+# 입출력 예
+# number	     k	    return
+# "1924"	     2	    "94"
+# "1231234"	     3	    "3234"
+# "4177252841"	 4      "775841"
+
+def solution(number, k):
+    stack = [number[0]]
+    for num in number[1:]:
+        while len(stack) > 0 and stack[-1] < num and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(num)
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
